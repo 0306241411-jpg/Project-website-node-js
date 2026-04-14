f06a86f18b62ba46b234c37dc9396ea312e24f12
VoNgocTien <0306241411@caothang.edu.vn>
2026-04-14 23:00:12 +0700

    Tạo trang login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3080;
 const use_module = require("./routes/home");
+const bcrypt = require("bcrypt");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -10,4 +11,5 @@ app.use("/", use_module);
 
 app.listen(PORT, () => {
   console.log(`Server: http://localhost:${PORT}`);
+  //console.log(`Admin: http://localhost:${PORT}/`);
 });
--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const mysql = require("mysql2");
 //render index
 router.get("/", (req, res) => {
   res.render("layout", {
@@ -15,16 +15,42 @@ router.get("/category", (req, res) => {
   });
 });
 
+//render single
 router.get("/single", (req, res) => {
   res.render("layout", {
     content: "single",
   });
 });
 
+//render contact
 router.get("/contact", (req, res) => {
   res.render("layout", {
     content: "contact",
   });
+});
+
+//render login
+router.get("/login", (req, res) => {
+  res.render("login/login_layout", {
+    content: "login",
+  });
+});
+
+//kết nối mysql
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_mysql",
+});
+
+//kiểm tra kết nối mysql
+db.connect((err) => {
+  if (err) {
+    console.error("Lỗi kết nối csdl. Chi tiết:" + err.stack);
+    return;
+  }
+  console.log("Đã kết nối csdl thành công! ");
 });
 
 module.exports = router;
